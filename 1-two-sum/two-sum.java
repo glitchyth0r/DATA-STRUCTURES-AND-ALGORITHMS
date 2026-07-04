b6e5c1eec70a5ba4a9621acd77e3c71748a7c9fa
Jatin singh <137102062+glitchythor@users.noreply.github.com>
2026-07-04 19:51:19 +0530

    Improve comments and formatting in two-sum.java
    Added comments for clarity and fixed formatting.

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
       
-      int ans[]=new int[2];
+      int ans[]=new int[2];//new array is crated.
       for(int i=0;i<nums.length-1;i++)
-      {
+      {                                   // transversing the array /
          for(int j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target)
+            if(nums[i]+nums[j]==target) //  checking the condition , 
             {
                 ans[0]=i;
                 ans[1]=j;
@@ -16,3 +16,5 @@ class Solution {
       return ans;
     }
 }
+
+//time complexity is O(n**2)
