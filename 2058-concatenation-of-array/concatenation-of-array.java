cfaebe12b5b35e6aa9e86b0e4b89259469109ace
Jatin singh <137102062+glitchythor@users.noreply.github.com>
2026-07-14 00:23:44 +0530

    Time: 1 ms (97.34%) | Memory: 47.2 MB (36.47%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -3,12 +3,12 @@ class Solution {
         int n=nums.length;
         int m=2*n;
         int ans[]=new int[m];
-        
-        for(int i=0;i<n;i++)
-        {
+
+        for(int i=0;i<n;i++){
             ans[i]=nums[i];
             ans[i+n]=nums[i];
         }
+
         return ans;
     }
 }
