e4ef65e8122d091c76e0788eb67ec3f2509c9ac6
Jatin <137102062+glitchyth0r@users.noreply.github.com>
2026-08-04 19:23:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (43.44%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -5,9 +5,9 @@ class Solution {
             digits[i]++;
             return digits;
           }
-          digits[i]=0;
+           digits[i]=0;
         }
-        int[] result=new int[digits.length+1];
+        int result[]=new int[digits.length+1];
         result[0]=1;
 
         return result;
