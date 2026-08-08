7b013b4bba4a2f71b42eb0a6a67d7e7c6b8805bd
Jatin <137102062+glitchyth0r@users.noreply.github.com>
2026-08-08 12:55:33 +0530

    Time: 2 ms (28.92%) | Memory: 47.5 MB (6.98%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -3,13 +3,14 @@ class Solution {
         int n=nums.length;
         int max=nums[0];
         int min=nums[0];
+
         for(int i=0;i<n;i++){
           max=Math.max(max,nums[i]);
           min=Math.min(min,nums[i]);
         }
-        int hash[]=new int[max-min+1];
+        int[] hash=new int[max-min+1];
         for(int i=0;i<n;i++){
-           hash[nums[i]-min]++;
+          hash[nums[i]-min]++;
         }
         for(int i=0;i<n;i++){
           if(hash[nums[i]-min]==1){
